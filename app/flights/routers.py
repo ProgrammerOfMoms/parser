889c8e2e223448726b58f2919757becee3d59f1b
David Tohunts <45290250+ProgrammerOfMoms@users.noreply.github.com>
2023-04-20 13:30:58 +0700

    add test router

--- app/flights/routers.py
+++ app/flights/routers.py
@@ -24,3 +24,8 @@ def create_flights(flight: FlightIn,
     new_flight = crud.create_flight(session, flight)
     session.commit()
     return new_flight
+
+
+@router.get("/ping")
+def ping() -> str:
+    return "pong"
